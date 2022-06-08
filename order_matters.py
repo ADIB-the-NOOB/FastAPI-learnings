4a47bc7fd792f81fa1196892ff79bbeec3f95fcb
Mohammed Adib <76277079+ADIB-the-NOOB@users.noreply.github.com>
2022-06-08 11:35:35 +0600

    Order matters(minor)

--- order_matters.py
+++ order_matters.py
@@ -6,6 +6,6 @@ app = FastAPI()
 async def read_users_me():
     return {"username": "the_current_user"}
 
-@app.get("/users/me")
-async def read_users_me():
-    return {"username": "the_current_user"}
+@app.get("/users/{user_id}")
+async def read_users(user_id: int):
+    return {"user_id": user_id}
